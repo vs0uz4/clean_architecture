b71226b22198f06a37789c5db50bc1bff5e2b042
vs0uz4 <vitor.rodrigues@gmail.com>
2024-10-30 02:54:00 -0300

    initial commit

---
+++ internal/infra/graph/resolver.go
@@ -0,0 +1,11 @@
+package graph
+
+import "github.com/vs0uz4/clean_architecture/internal/usecase"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	CreateOrderUseCase usecase.CreateOrderUseCase
+}
