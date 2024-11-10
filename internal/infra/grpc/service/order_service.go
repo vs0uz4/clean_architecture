cc57c7afe452b2aeacd7b4578d944c35c6412610
vs0uz4 <vitor.rodrigues@gmail.com>
2024-11-10 00:35:37 -0300

    create listorderservice and register

--- internal/infra/grpc/service/order_service.go
+++ internal/infra/grpc/service/order_service.go
@@ -6,20 +6,23 @@ import (
 	"github.com/vs0uz4/clean_architecture/internal/dto"
 	"github.com/vs0uz4/clean_architecture/internal/infra/grpc/pb"
 	"github.com/vs0uz4/clean_architecture/internal/usecase"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 type OrderService struct {
 	pb.UnimplementedOrderServiceServer
 	CreateOrderUseCase usecase.CreateOrderUseCase
+	ListOrderUseCase   usecase.ListOrderUseCase
 }
 
-func NewOrderService(createOrderUseCase usecase.CreateOrderUseCase) *OrderService {
+func NewOrderService(createOrderUseCase usecase.CreateOrderUseCase, listOrderUseCase usecase.ListOrderUseCase) *OrderService {
 	return &OrderService{
 		CreateOrderUseCase: createOrderUseCase,
+		ListOrderUseCase:   listOrderUseCase,
 	}
 }
 
-func (s *OrderService) CreateOrder(ctx context.Context, in *pb.CreateOrderRequest) (*pb.CreateOrderResponse, error) {
+func (s *OrderService) CreateOrder(ctx context.Context, in *pb.CreateOrderRequest) (*pb.OrderResponse, error) {
 	dto := dto.OrderInputDTO{
 		ID:    in.Id,
 		Price: float64(in.Price),
@@ -29,11 +32,35 @@ func (s *OrderService) CreateOrder(ctx context.Context, in *pb.CreateOrderReques
 	if err != nil {
 		return nil, err
 	}
-	return &pb.CreateOrderResponse{
+	return &pb.OrderResponse{
 		Id:         output.ID,
 		Price:      float32(output.Price),
 		Tax:        float32(output.Tax),
 		FinalPrice: float32(output.FinalPrice),
 		CreatedAt:  output.CreatedAt,
 	}, nil
+}
+
+func (s *OrderService) ListOrders(ctx context.Context, in *emptypb.Empty) (*pb.ListOrdersResponse, error) {
+	output, err := s.ListOrderUseCase.Execute()
+	if err != nil {
+		return nil, err
+	}
+
+	orders := make([]*pb.OrderResponse, len(output))
+	for i, o := range output {
+		orders[i] = &pb.OrderResponse{
+			Id:         o.ID,
+			Price:      float32(o.Price),
+			Tax:        float32(o.Tax),
+			FinalPrice: float32(o.FinalPrice),
+			CreatedAt:  o.CreatedAt,
+		}
+	}
+
+	if len(orders) == 0 {
+		orders = []*pb.OrderResponse{}
+	}
+
+	return &pb.ListOrdersResponse{Orders: orders}, nil
 }
